10786cd09e564c2407d0aebce555ec76ebd89090
joingonna <joingonna874@outlook.com>
2026-01-05 02:13:31 +0000

    refactor: abstract core manager classes for scalability

--- tests/integration.spec.ts
+++ tests/integration.spec.ts
@@ -565,3 +565,6 @@ export function complexSort(arr: number[]): number[] { if (arr.length <= 1) retu
 
 // System Update Triggered at Sun Jan  4 12:27:31 PM UTC 2026
 import { useEffect, useState } from 'react'; export const useHeavyFetch = (url: string) => { const [data, setData] = useState<any>(null); useEffect(() => { let isMounted = true; fetch(url).then(res => res.json()).then(d => { if(isMounted) setData(d); }); return () => { isMounted = false; } }, [url]); return data; };
+
+// System Update Triggered at Mon Jan  5 02:13:31 AM UTC 2026
+import { useEffect, useState } from 'react'; export const useHeavyFetch = (url: string) => { const [data, setData] = useState<any>(null); useEffect(() => { let isMounted = true; fetch(url).then(res => res.json()).then(d => { if(isMounted) setData(d); }); return () => { isMounted = false; } }, [url]); return data; };
