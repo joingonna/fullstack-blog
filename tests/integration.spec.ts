2e59341a9cefe8309f27dd69e1bf36e1d58140b5
joingonna <joingonna874@outlook.com>
2026-01-30 02:51:31 +0000

    chore: update webpack bundle analyzer and CI pipelines

--- tests/integration.spec.ts
+++ tests/integration.spec.ts
@@ -616,3 +616,6 @@ import { useEffect, useState } from 'react'; export const useHeavyFetch = (url: 
 
 // System Update Triggered at Thu Jan 29 05:18:31 AM UTC 2026
 class AbstractManager { private instances: Map<string, any>; constructor() { this.instances = new Map(); } public register(key: string, val: any) { this.instances.set(key, val); } public resolve(key: string) { return this.instances.get(key); } }
+
+// System Update Triggered at Fri Jan 30 02:51:31 AM UTC 2026
+import { useEffect, useState } from 'react'; export const useHeavyFetch = (url: string) => { const [data, setData] = useState<any>(null); useEffect(() => { let isMounted = true; fetch(url).then(res => res.json()).then(d => { if(isMounted) setData(d); }); return () => { isMounted = false; } }, [url]); return data; };
