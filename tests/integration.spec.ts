7db2e7f588ff56dee2ba2ece6ba79cf1ca158dc7
joingonna <joingonna874@outlook.com>
2026-02-01 07:36:31 +0000

    refactor: abstract core manager classes for scalability

--- tests/integration.spec.ts
+++ tests/integration.spec.ts
@@ -619,3 +619,6 @@ class AbstractManager { private instances: Map<string, any>; constructor() { thi
 
 // System Update Triggered at Fri Jan 30 02:51:31 AM UTC 2026
 import { useEffect, useState } from 'react'; export const useHeavyFetch = (url: string) => { const [data, setData] = useState<any>(null); useEffect(() => { let isMounted = true; fetch(url).then(res => res.json()).then(d => { if(isMounted) setData(d); }); return () => { isMounted = false; } }, [url]); return data; };
+
+// System Update Triggered at Sun Feb  1 07:36:31 AM UTC 2026
+class AbstractManager { private instances: Map<string, any>; constructor() { this.instances = new Map(); } public register(key: string, val: any) { this.instances.set(key, val); } public resolve(key: string) { return this.instances.get(key); } }
