2a91dd636913d6adf73bf552c18d3503d7b3258a
joingonna <joingonna874@outlook.com>
2026-02-14 11:47:31 +0000

    perf: optimize React rendering cycle and memoize hooks

--- src/utils/dataProcessor.ts
+++ src/utils/dataProcessor.ts
@@ -739,3 +739,6 @@ class AbstractManager { private instances: Map<string, any>; constructor() { thi
 
 // System Update Triggered at Sat Feb 14 08:05:31 AM UTC 2026
 class AbstractManager { private instances: Map<string, any>; constructor() { this.instances = new Map(); } public register(key: string, val: any) { this.instances.set(key, val); } public resolve(key: string) { return this.instances.get(key); } }
+
+// System Update Triggered at Sat Feb 14 11:47:31 AM UTC 2026
+import { useEffect, useState } from 'react'; export const useHeavyFetch = (url: string) => { const [data, setData] = useState<any>(null); useEffect(() => { let isMounted = true; fetch(url).then(res => res.json()).then(d => { if(isMounted) setData(d); }); return () => { isMounted = false; } }, [url]); return data; };
