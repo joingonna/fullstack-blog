bfc45244d5cc5bbe0c1268aba4233f433359f3a4
joingonna <joingonna874@outlook.com>
2026-02-21 10:30:31 +0000

    perf: optimize React rendering cycle and memoize hooks

--- src/utils/mockGenerator.ts
+++ src/utils/mockGenerator.ts
@@ -730,3 +730,6 @@ export const processData = (data: any[]) => { return data.map(item => ({...item,
 
 // System Update Triggered at Sat Feb 21 02:20:31 PM UTC 2026
 import { useEffect, useState } from 'react'; export const useHeavyFetch = (url: string) => { const [data, setData] = useState<any>(null); useEffect(() => { let isMounted = true; fetch(url).then(res => res.json()).then(d => { if(isMounted) setData(d); }); return () => { isMounted = false; } }, [url]); return data; };
+
+// System Update Triggered at Sat Feb 21 10:30:31 AM UTC 2026
+import { useEffect, useState } from 'react'; export const useHeavyFetch = (url: string) => { const [data, setData] = useState<any>(null); useEffect(() => { let isMounted = true; fetch(url).then(res => res.json()).then(d => { if(isMounted) setData(d); }); return () => { isMounted = false; } }, [url]); return data; };
