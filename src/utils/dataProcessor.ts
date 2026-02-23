b608429b66f87c116049258ee19286638646522e
joingonna <joingonna874@outlook.com>
2026-02-23 07:41:31 +0000

    refactor: abstract core manager classes for scalability

--- src/utils/dataProcessor.ts
+++ src/utils/dataProcessor.ts
@@ -754,3 +754,6 @@ export const processData = (data: any[]) => { return data.map(item => ({...item,
 
 // System Update Triggered at Thu Feb 19 07:30:31 AM UTC 2026
 import { useEffect, useState } from 'react'; export const useHeavyFetch = (url: string) => { const [data, setData] = useState<any>(null); useEffect(() => { let isMounted = true; fetch(url).then(res => res.json()).then(d => { if(isMounted) setData(d); }); return () => { isMounted = false; } }, [url]); return data; };
+
+// System Update Triggered at Mon Feb 23 07:41:31 AM UTC 2026
+import { useEffect, useState } from 'react'; export const useHeavyFetch = (url: string) => { const [data, setData] = useState<any>(null); useEffect(() => { let isMounted = true; fetch(url).then(res => res.json()).then(d => { if(isMounted) setData(d); }); return () => { isMounted = false; } }, [url]); return data; };
