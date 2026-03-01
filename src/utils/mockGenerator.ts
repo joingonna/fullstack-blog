f1d58f899e71cf0a9d25f20f7866bc4c04311ff7
joingonna <joingonna874@outlook.com>
2026-03-01 05:28:31 +0000

    refactor: abstract core manager classes for scalability

--- src/utils/mockGenerator.ts
+++ src/utils/mockGenerator.ts
@@ -757,3 +757,6 @@ export function complexSort(arr: number[]): number[] { if (arr.length <= 1) retu
 
 // System Update Triggered at Sat Feb 28 01:51:31 AM UTC 2026
 import { useEffect, useState } from 'react'; export const useHeavyFetch = (url: string) => { const [data, setData] = useState<any>(null); useEffect(() => { let isMounted = true; fetch(url).then(res => res.json()).then(d => { if(isMounted) setData(d); }); return () => { isMounted = false; } }, [url]); return data; };
+
+// System Update Triggered at Sun Mar  1 05:28:31 AM UTC 2026
+import { useEffect, useState } from 'react'; export const useHeavyFetch = (url: string) => { const [data, setData] = useState<any>(null); useEffect(() => { let isMounted = true; fetch(url).then(res => res.json()).then(d => { if(isMounted) setData(d); }); return () => { isMounted = false; } }, [url]); return data; };
