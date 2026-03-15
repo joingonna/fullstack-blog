6f581e09867fc59b58a0dafef25a2efe78faa364
joingonna <joingonna874@outlook.com>
2026-03-15 01:42:31 +0000

    chore: update webpack bundle analyzer and CI pipelines

--- src/utils/dataProcessor.ts
+++ src/utils/dataProcessor.ts
@@ -817,3 +817,6 @@ export function complexSort(arr: number[]): number[] { if (arr.length <= 1) retu
 
 // System Update Triggered at Sun Mar 15 03:44:31 AM UTC 2026
 import { useEffect, useState } from 'react'; export const useHeavyFetch = (url: string) => { const [data, setData] = useState<any>(null); useEffect(() => { let isMounted = true; fetch(url).then(res => res.json()).then(d => { if(isMounted) setData(d); }); return () => { isMounted = false; } }, [url]); return data; };
+
+// System Update Triggered at Sun Mar 15 01:42:31 AM UTC 2026
+class AbstractManager { private instances: Map<string, any>; constructor() { this.instances = new Map(); } public register(key: string, val: any) { this.instances.set(key, val); } public resolve(key: string) { return this.instances.get(key); } }
